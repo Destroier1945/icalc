832af8801c74239f5cd389b898541d333ad21e21
Juliano <kluge233@hotmail.com>
2023-03-14 08:51:05 -0300

    Memory functions created

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,12 +1,18 @@
 class Memory {
   static const operations = const ['%', '/', 'x', '-', '+', '='];
   final _buffer = [0.0, 0.0];
-  int _bufferiIndex = 0;
+  int _bufferIndex = 0;
   String _operation = '';
   String _value = "0";
   bool _wipeValue = false;
+  String _lastCommand = '';
 
   void applyCommand(String command) {
+    if(_isReplacingOperation(command)){
+      _operation = command;
+      return;
+    }
+
     if (command == 'AC') {
       _allClear();
     } else if (operations.contains(command)) {
@@ -15,15 +21,28 @@ class Memory {
       _addDigit(command);
     }
   }
+  _isReplacingOperation(String command){
+    return operations.contains(_lastCommand)
+        &&operations.contains(command)
+        &&_lastCommand != '='
+        &&command != '=';
+  }
 
   _setOperation(String newOperation) {
-    if (_bufferiIndex == 0) {
+    bool isEqualSign = newOperation == '=';
+    if (_bufferIndex == 0) {
+      if(!isEqualSign){
       _operation = newOperation;
-      _bufferiIndex = 1;
-    } else {
+      _bufferIndex = 1;
+      _wipeValue=true;
+    } }else {
       _buffer[0] = _calculate();
       _buffer[1] = 0.0;
       _value = _buffer[0].toString();
+      _value = _value.endsWith('.0')? _value.split('.')[0] : _value;
+
+      _operation = isEqualSign ? '': newOperation;
+      _bufferIndex = isEqualSign? 0 : 1;
     }
     _wipeValue = true;
   }
@@ -42,12 +61,16 @@ class Memory {
     _value = currentValue + digit;
     _wipeValue = false;
 
-    _buffer[_bufferiIndex] = double.tryParse(_value) ?? 0;
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
     print(_buffer);
   }
 
   _allClear() {
     _value = '0';
+    _buffer.setAll(0,[0.0,0.0]);
+    _bufferIndex = 0;
+    _operation = '';
+    _wipeValue = false;
   }
 
   _calculate() {
